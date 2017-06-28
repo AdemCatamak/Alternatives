3c9a67eccec5f2c1ed8c2e4332d4bb8ccfc58a71
Adem Çatamak <ademcatamak@gmail.com>
2017-06-29 00:04:04 +0300

    Test functions seperate to different class files

---
+++ AdemCatamak.Utilities.UnitTest/ExtensionTest/ToDoubleTest.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AdemCatamak.Utilities.UnitTest.ExtensionTest
+{
+    [TestClass]
+    public class ToDoubleTest
+    {
+
+        [TestMethod]
+        [ExpectedException(typeof(NullReferenceException), "Argument not null")]
+        public void AdemCatamak_Utilities_UnitTest_ExtensionsTest__ToDouble_Null()
+        {
+            ((object)null).ToDouble();
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(FormatException), "Value cannot be converted")]
+        public void AdemCatamak_Utilities_UnitTest_ExtensionsTest__ToDouble_Alphabet()
+        {
+            const string data = "123a123.24";
+
+
+            data.ToDouble();
+        }
+
+        [TestMethod]
+        public void AdemCatamak_Utilities_UnitTest_ExtensionsTest__ToDouble_WithComma()
+        {
+            const double expected = 12.5;
+            const string data = "12,5";
+
+
+            double actual = data.ToDouble();
+
+
+            Assert.AreEqual(expected, actual, $"{actual} value is not expected");
+        }
+
+        [TestMethod]
+        public void AdemCatamak_Utilities_UnitTest_ExtensionsTest__ToDouble_WithDot()
+        {
+            const double expected = 1215;
+            const string data = "12.15";
+
+
+            double actual = data.ToDouble();
+
+
+            Assert.AreEqual(expected, actual, $"{actual} value is not expected");
+        }
+    }
+}
