a5d3e0637fc960fe51d0f8c358a8de7ebb4c224b
Adem Çatamak <ademcatamak@gmail.com>
2017-07-27 00:26:49 +0300

    Add BusinessException class

--- Alternatives.UnitTest/WebApiConsumerTest.cs
+++ Alternatives.UnitTest/WebApiConsumerTest.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Alternatives.CustomExceptions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Alternatives.UnitTest
 {
--- Alternatives/WebApiConsumer.cs
+++ Alternatives/WebApiConsumer.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Net.Http;
 using System.Net.Http.Headers;
+using Alternatives.CustomExceptions;
 
 namespace Alternatives
 {
