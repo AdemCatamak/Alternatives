3dc2bdf75ba183d9e9d56b024346b6a26040f163
Adem Catamak <ademcatamak@gmail.com>
2018-01-03 22:32:47 +0300

    Custom Exception Types added

---
+++ Alternatives/CustomExceptions/CustomApiException.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Net;
+
+namespace Alternatives.CustomExceptions
+{
+    public class CustomApiException : Exception
+    {
+        public string FriendlyMessage => Message;
+        public HttpStatusCode ReturnHttpStatusCode { get; }
+
+        public CustomApiException(string friendlyMessage, HttpStatusCode returnStatusCode, Exception innerException = null)
+            : base(friendlyMessage, innerException)
+        {
+            ReturnHttpStatusCode = returnStatusCode;
+        }
+    }
+}
---
+++ Alternatives/CustomExceptions/CustomFriendlyException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Alternatives.CustomExceptions
+{
+    public class CustomFriendlyException : Exception
+    {
+        public string FriendlyMessage => Message;
+
+        public CustomFriendlyException(string friendlyMessage, Exception innerException = null)
+            : base(friendlyMessage, innerException)
+        {
+        }
+    }
+}
