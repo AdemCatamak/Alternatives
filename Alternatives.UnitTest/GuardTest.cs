319ceeac46a13fa42ef68d558435c4ba03b1d4be
Adem Catamak <ademcatamak@gmail.com>
2018-06-25 23:54:19 +0300

    guard extensions removed

--- Alternatives.UnitTest/GuardTest.cs
+++ Alternatives.UnitTest/GuardTest.cs
@@ -1,29 +1,47 @@
-﻿using Xunit;
+﻿using System;
+using Xunit;
 
 namespace Alternatives.UnitTest
 {
     public class GuardTest
     {
         [Fact]
-        public void IfItIsNull_IfObjectIsNull_ActionWillBeExecuted()
+        public void IsNull_IfObjectIsNull_ActionWillBeExecuted()
         {
             int value = 0;
             object obj = null;
 
-            obj.IsNull(() => value++);
+            // ReSharper disable once ExpressionIsAlwaysNull
+            Guard.IsNull(obj, () => value++);
 
             Assert.Equal(1, value);
         }
 
         [Fact]
-        public void IfItIsNull_IfObjectIsNotNull_ActionWillNotBeExecuted()
+        public void IsNull_IfObjectIsNotNull_ActionWillNotBeExecuted()
         {
             int value = 0;
 
-            5.IsNull(() => value++);
+            Guard.IsNull(42, () => value++);
 
             Assert.Equal(0, value);
         }
+
+        [Fact]
+        public void IsNull_IfObjectIsNull_ExceptionIsThrown()
+        {
+            object obj = null;
+
+            // ReSharper disable once ExpressionIsAlwaysNull
+            Assert.Throws<TimeoutException>(() => Guard.IsNull(obj, new TimeoutException("Test")));
+        }
+
+        [Fact]
+        public void IsNull_IfObjectIsNotNull_ExceptionIsNotThrown()
+        {
+            Guard.IsNull(42, new TimeoutException("Test"));
+        }
+
 
         [Fact]
         public void IsTrue_IfConditionIsTrue_ActionWillBeWork()
@@ -31,7 +49,7 @@ namespace Alternatives.UnitTest
             const bool condition = true;
             bool operationGetHit = false;
 
-            condition.IsTrue(() => operationGetHit = true);
+            Guard.IsTrue(condition, () => operationGetHit = true);
 
             Assert.True(operationGetHit);
         }
@@ -42,10 +60,27 @@ namespace Alternatives.UnitTest
             const bool condition = false;
             bool operationGetHit = false;
 
-            condition.IsTrue(() => operationGetHit = true);
+            Guard.IsTrue(condition, () => operationGetHit = true);
 
             Assert.False(operationGetHit);
         }
+
+        [Fact]
+        public void IsTrue_IfConditionIsTrue_ExceptionIsThrown()
+        {
+            const bool condition = true;
+
+            Assert.Throws<TimeoutException>(() => Guard.IsTrue(condition, new TimeoutException()));
+        }
+
+        [Fact]
+        public void IsTrue_IfConditionIsFalse_ExceptionIsNotThrown()
+        {
+            const bool condition = false;
+
+            Guard.IsTrue(condition, new ArgumentException());
+        }
+
 
         [Fact]
         public void IsFalse_IfConditionIsTrue_ActionWillNotBeWork()
@@ -53,7 +88,7 @@ namespace Alternatives.UnitTest
             const bool valid = true;
             bool operationGetHit = false;
 
-            valid.IsFalse(() => operationGetHit = true);
+            Guard.IsFalse(valid, () => operationGetHit = true);
 
             Assert.False(operationGetHit);
         }
@@ -64,29 +99,46 @@ namespace Alternatives.UnitTest
             const bool valid = false;
             bool operationGetHit = false;
 
-            valid.IsFalse(() => operationGetHit = true);
+            Guard.IsFalse(valid, () => operationGetHit = true);
 
             Assert.True(operationGetHit);
         }
 
         [Fact]
-        public void IsConditionMeet_IfConditionIsTrue_ActionWillBeWork()
+        public void IsFalse_IfConditionIsTrue_ExceptionIsNotThrown()
+        {
+            const bool valid = true;
+
+            Guard.IsFalse(valid, new ArgumentException());
+        }
+
+        [Fact]
+        public void IsFalse_IfConditionIsFalse_ExceptionIsThrown()
+        {
+            const bool valid = false;
+
+            Assert.Throws<AccessViolationException>(() => Guard.IsFalse(valid, new AccessViolationException()));
+        }
+
+
+        [Fact]
+        public void IfConditionMeet_IfConditionIsTrue_ActionWillBeWork()
         {
             const int value = 5;
             bool operationGetHit = false;
 
-            Guard.IsConditionMeet(() => value > 0, () => operationGetHit = true);
+            Guard.IfConditionMeet(() => value > 0, () => operationGetHit = true);
 
             Assert.True(operationGetHit);
         }
 
         [Fact]
-        public void IsConditionMeet_IfConditionIsFalse_ActionWillNotBeWork()
+        public void IfConditionMeet_IfConditionIsFalse_ActionWillNotBeWork()
         {
             const int value = 5;
             bool operationGetHit = false;
 
-            Guard.IsConditionMeet(() => value < 0, () => operationGetHit = true);
+            Guard.IfConditionMeet(() => value < 0, () => operationGetHit = true);
 
             Assert.False(operationGetHit);
         }
--- Alternatives/Guard.cs
+++ Alternatives/Guard.cs
@@ -4,22 +4,34 @@ namespace Alternatives
 {
     public static class Guard
     {
-        public static void IsNull(this object instance, Action action)
+        public static void IsNull<T>(object instance, T exception) where T : Exception
+        {
+            IsNull(instance, () => throw exception);
+        }
+        public static void IsNull(object instance, Action action)
         {
             IfConditionMeet(instance == null, action);
         }
 
-        public static void IsTrue(this bool condition, Action action)
+        public static void IsTrue<T>(bool condition, T exception) where T : Exception
+        {
+            IsTrue(condition, () => throw exception);
+        }
+        public static void IsTrue(bool condition, Action action)
         {
             IfConditionMeet(condition, action);
         }
 
-        public static void IsFalse(this bool condition, Action action)
+        public static void IsFalse<T>(bool condition, T exception) where T : Exception
+        {
+            IsFalse(condition, () => throw exception);
+        }
+        public static void IsFalse(bool condition, Action action)
         {
             IfConditionMeet(!condition, action);
         }
 
-        public static void IsConditionMeet(Func<bool> conditionFunc, Action action)
+        public static void IfConditionMeet(Func<bool> conditionFunc, Action action)
         {
             bool result = conditionFunc();
             IfConditionMeet(result, action);
